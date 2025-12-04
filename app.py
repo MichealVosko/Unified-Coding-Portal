f98364bb7c510f3b226f04a73cbc775342450ff6
Im-Moazzam <moazzamaleem788@gmail.com>
2025-12-04 23:53:28 +0500

    Applied changes in app as well for name correction of mental health ambition

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from robertson.robertson_app import run as run_robertson_app
 from cognitive.cognitive_app import run as run_cognitive_app
-from medical_wealth_ambition.medical_wealth_ambition_app import run as run_medical_wealth_ambition_app
+from mental_wealth_ambition.mental_wealth_ambition_app import run as run_mental_wealth_ambition_app
 
 st.set_page_config(page_title="Coding Portal")
 
-tab = st.sidebar.radio("Select App", ["Robertson", "Cognitive", "Medical Wealth Ambition"])
+tab = st.sidebar.radio("Select App", ["Robertson", "Cognitive", "Mental Wealth Ambition"])
 
 if tab == "Robertson":
     run_robertson_app()
 elif tab == "Cognitive":
     run_cognitive_app()
-elif tab == "Medical Wealth Ambition":
-    run_medical_wealth_ambition_app()
+elif tab == "Mental Wealth Ambition":
+    run_mental_wealth_ambition_app()
